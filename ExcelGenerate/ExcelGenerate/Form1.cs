56272b5e91486c45ffd8c067eccbbc9f6115353c
iuwts3 <adam.grets@stud.uni-corvinus.hu>
2022-10-05 17:15:13 +0200

    commit 7-8

--- ExcelGenerate/ExcelGenerate/Form1.cs
+++ ExcelGenerate/ExcelGenerate/Form1.cs
@@ -26,6 +26,95 @@ namespace ExcelGenerate
             InitializeComponent();
             LoadData();
             CreateExcel();
+            CreateTable();
+        }
+
+        private void CreateTable()
+        {
+            string[] headers = new string[] {
+                 "Kód",
+                 "Eladó",
+                 "Oldal",
+                 "Kerület",
+                 "Lift",
+                 "Szobák száma",
+                 "Alapterület (m2)",
+                 "Ár (mFt)",
+                 "Négyzetméter ár (Ft/m2)" };
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                xlSheet.Cells[1, 1] = headers[0];
+                xlSheet.Cells[1, 2] = headers[1];
+                xlSheet.Cells[1, 3] = headers[2];
+                xlSheet.Cells[1, 4] = headers[3];
+                xlSheet.Cells[1, 5] = headers[4];
+                xlSheet.Cells[1, 6] = headers[5];
+                xlSheet.Cells[1, 7] = headers[6];
+                xlSheet.Cells[1, 8] = headers[7];
+                xlSheet.Cells[1, 9] = headers[8];
+            }
+
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+
+                counter++;
+            }
+
+            xlSheet.get_Range(
+             GetCell(2, 1),
+             GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            Excel.Range tableRange = xlSheet.get_Range(GetCell(2, 1), GetCell(values.Length, headers.Length));
+            tableRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick); //tábla szegély
+
+            Excel.Range firstColummnRange = xlSheet.get_Range(GetCell(2, 1), GetCell(values.Length, 1));
+            //első oszlop formázásai
+            firstColummnRange.Font.Bold = true;
+            firstColummnRange.Interior.Color = Color.LightYellow;
+
+            int lastRowID = xlSheet.UsedRange.Rows.Count;
+            Excel.Range lastColummnRange = xlSheet.get_Range(GetCell(2, lastRowID), GetCell(values.Length, lastRowID));
+            //utolsó oszlop formázásai --- még nem jó
+            lastColummnRange.Interior.Color = Color.LightGreen;
+        }
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
         }
 
         private void CreateExcel()
@@ -42,7 +131,7 @@ namespace ExcelGenerate
                 xlSheet = xlWB.ActiveSheet;
 
                 // Tábla létrehozása
-                //CreateTable();
+                CreateTable();
 
                 // Control átadása a felhasználónak
                 xlApp.Visible = true;
@@ -63,7 +152,7 @@ namespace ExcelGenerate
 
         private void LoadData()
         {
-            List<Flat> Flats = context.Flats.ToList();
+            Flats = context.Flats.ToList();
         }
     }
 }
