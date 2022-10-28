27e50b641d67c24cf686910e341af208a0a62b05
iuwts3 <adam.grets@stud.uni-corvinus.hu>
2022-10-28 14:12:38 +0200

    4. feladat - Játék absztrakció kezelése

---
+++ 06_gyak/06_gyak/Abstractions/Toy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06_gyak.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += Toy_Paint;
+        }
+
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected abstract void DrawImage(Graphics graphics);
+    }
+}
