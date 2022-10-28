41f11eb311a36d6316eab382726638f7e19eca1c
iuwts3 <adam.grets@stud.uni-corvinus.hu>
2022-10-28 14:20:06 +0200

    6. feladat - Autó létrehozása

---
+++ 06_gyak/06_gyak/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using _06_gyak.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_gyak.Entities
+{
+    internal class Car : Toy
+    {
+        protected override void DrawImage(Graphics graphics)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            graphics.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
