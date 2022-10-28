322fdf82819fb24cbc47e6d8669454962ff69df0
iuwts3 <adam.grets@stud.uni-corvinus.hu>
2022-10-28 13:36:53 +0200

    1. feladat

---
+++ 06_gyak/06_gyak/Entities/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _06_gyak.Entities
+{
+    internal class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += Ball_Paint;
+        }
+
+        private void MoveBall()
+        {
+            Left += 1;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+
+        protected void DrawImage(Graphics graphics)
+        {
+            graphics.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+    }
+}
