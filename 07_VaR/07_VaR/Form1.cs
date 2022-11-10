17ab33471349abfcbaa42c3b426fa22a433ee1fa
iuwts3 <adam.grets@stud.uni-corvinus.hu>
2022-11-10 10:49:24 +0100

    5. Portfólió tárolása a memóriában

--- 07_VaR/07_VaR/Form1.cs
+++ 07_VaR/07_VaR/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _07_VaR.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,11 +16,23 @@ namespace _07_VaR
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
 
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
